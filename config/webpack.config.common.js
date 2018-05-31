1fa41e8732f663dddf9530f7430c9e173ed59e6a
id10bat <my_nutthapat-26_@hotmail.com>
2018-06-01 02:13:15 +0700

    i'm tester

---
+++ config/webpack.config.common.js
@@ -0,0 +1,91 @@
+const { resolve, join } = require('path');
+const webpack = require('webpack');
+
+
+
+module.exports = {
+  target: 'web',
+  entry: ['./src/client/index.js'],
+  output: {
+    publicPath: '/',
+    path: resolve(__dirname, '..', 'build', 'client'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin(['NODE_ENV'])
+  ],
+  resolve: {
+    modules: ['node_modules', join('src', 'client')]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  stats: {
+    assetsSort: '!size',
+    children: false,
+    chunks: false,
+    colors: true,
+    entrypoints: false,
+    modules: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx$/,
+        use: 'jsx-loader'
+      },
+      {
+        test: /\.(ts|tsx)?$/,
+        use: "ts-loader"
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
+  },
+};
---
+++ src/client/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/stats")
+      .then(response => response.json())
+      .then(data => this.setState({ isLoaded: true, items: data.servers }))
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+
+    return (
+      <div>
+        {items.map(item =>
+          <div key={item.id}>
+            <h1>{item.name}</h1>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+
+export default App;
